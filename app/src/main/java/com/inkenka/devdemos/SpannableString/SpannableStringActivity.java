fb57961030c2038a088adc94f327237db0e633ed
InKenKa <yinfork@foxmail.com>
2017-09-27 11:34:55 +0800

    添加支持垂直居中的ImageSpan
    Signed-off-by: InKenKa <yinfork@foxmail.com>

--- app/src/main/java/com/inkenka/devdemos/SpannableString/SpannableStringActivity.java
+++ app/src/main/java/com/inkenka/devdemos/SpannableString/SpannableStringActivity.java
@@ -22,17 +22,17 @@ public class SpannableStringActivity extends AppCompatActivity {
 
         SpannableStringBuilder sb = new SpannableStringBuilder();
 
-        sb.append(" "); // 占位
-        Drawable d = getResources().getDrawable(R.drawable.ic_question);
-        d.setBounds(0, 0, Utils.covertDp2Px(this, 12), Utils.covertDp2Px(this, 12));    // 关键这句，没有就显示不了
-        sb.setSpan(new ImageSpan(d, ImageSpan.ALIGN_BASELINE), sb.length()-1, sb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        sb.append("  "); // 占位
+        Drawable drawable = getResources().getDrawable(R.drawable.ic_question);
+        drawable.setBounds(0, 0, Utils.covertDp2Px(this, 12), Utils.covertDp2Px(this, 12));   // 关键这句，没有就显示不了
+        sb.setSpan(new VerticalImageSpan(drawable), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         sb.append("Hello World!");
 
-        sb.append(" "); // 占位
-        d = getResources().getDrawable(R.drawable.ic_arrow);
-        d.setBounds(0, 0, Utils.covertDp2Px(this, 12), Utils.covertDp2Px(this, 12));    // 关键这句，没有就显示不了
-        sb.setSpan(new ImageSpan(d, ImageSpan.ALIGN_BASELINE), sb.length()-1, sb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        sb.append("  "); // 占位
+        drawable = getResources().getDrawable(R.drawable.ic_arrow);
+        drawable.setBounds(0, 0, Utils.covertDp2Px(this, 8), Utils.covertDp2Px(this, 8));    // 关键这句，没有就显示不了
+        sb.setSpan(new VerticalImageSpan(drawable), sb.length()-1, sb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         textView.setText(sb);
     }
