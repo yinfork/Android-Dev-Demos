2f89615c5c7820586883361ceb1c3a27332e64c5
InKenKa <yinfork@foxmail.com>
2018-01-06 14:51:20 +0800

    修改获取CPU型号逻辑

--- app/src/main/java/com/inkenka/devdemos/cpuinfo/CpuInfoDemoActivity.java
+++ app/src/main/java/com/inkenka/devdemos/cpuinfo/CpuInfoDemoActivity.java
@@ -1,7 +1,9 @@
 package com.inkenka.devdemos.cpuinfo;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,21 +22,27 @@ public class CpuInfoDemoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cpuinfo_demo);
 
         TextView tvInfo = (TextView) findViewById(R.id.tv_info);
-        tvInfo.setText(getCpuName());
+        tvInfo.setText(getCPUModel());
     }
 
-    public static String getCpuName() {
+    public static String getCPUModel() {
+        String cpuModel = null;
+
+        // 通过读取cpuinfo文件获取CPU型号
         try {
             FileReader fr = new FileReader("/proc/cpuinfo");
             BufferedReader br = new BufferedReader(fr);
             String line;
             do {
                 line = br.readLine();
-                if (line.contains("Hardware")){
+                // 找到Hardware这一行
+                if (!TextUtils.isEmpty(line) && line.contains("Hardware")){
                     String[] array = line.split(" ");
+                    // 这一行最后显示的就是CPU型号
                     if (array != null && array.length > 0){
-                        return array[array.length -1].trim();
+                        cpuModel = array[array.length -1].trim().toUpperCase();
                     }
+                    break;
                 }
             } while (line != null);
         } catch (FileNotFoundException e) {
@@ -42,7 +50,12 @@ public class CpuInfoDemoActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+
+        // 部分手机无法通过cpuinfo获取芯片名，则返回Build.BOARD
+        if (TextUtils.isEmpty(cpuModel)){
+            cpuModel = Build.BOARD;
+        }
+        return cpuModel;
     }
 
 }
